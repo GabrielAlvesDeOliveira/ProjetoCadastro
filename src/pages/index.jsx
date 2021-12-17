efe202fd67e314d941421da81d9e569ecab2f83a
GabrielAlvesdeOliveira <alvesg2003@gmail.com>
2021-12-17 09:27:38 -0300

    using API at next.js

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -13,7 +13,11 @@ export default function Inicio(){
       <Navegador texto="Estiloso" destino="/estiloso"/>
       <Navegador texto="Exemplo" destino="/exemplo" cor="darkviolet"/>
       <Navegador texto="JSX" destino="/jsx" cor="crimson"/>
-      <Navegador texto="Navegacao #01" destino="/navegacao" cor="green"/>
+      <Navegador texto="Navegacao #01" destino="/navegacao/" cor="green"/>
+      <Navegador texto="Navegacao #02" destino="/cliente/codigo" cor="blue"/>
+      <Navegador texto="Componente com Estado" destino="/estado" cor="#a45b71"/>
+      <Navegador texto="Integracao com API #01" destino="/integracao_1" cor="#42a9a9"/>
+
     </div>
   )
 }
